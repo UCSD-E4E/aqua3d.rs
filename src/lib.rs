2f8dc4ef64b3cd73b68746075100074555ba3f8e
Avik Ghosh <a.ghosh24@outlook.com>
2024-11-18 13:56:01 -0800

    feat: clearing up test with more reasonable data. tests still manual

--- src/lib.rs
+++ src/lib.rs
@@ -20,16 +20,16 @@ mod tests {
     fn test_construct_neighborhood_map() {
         // let depth_matrix = DMatrix::<f32>::from_data(data);
         let depth_matrix = DMatrix::<f32>::from_row_slice(5, 5, &[
-            1.0001, 0.0, 0.0, 1.0004, 1.0005,  // First row (values close to each other)
-            2.0,    0.0,    0.0,    5.0,    6.0,    // Second row
-            7.0,    8.0,    9.0,    0.0,   11.0,   // Third row
-            12.0,   13.0,   0.0,   15.0,   16.0,   // Fourth row
-            17.0,   18.0,   19.0,   20.0,   21.0    // Fifth row
+            0.001,  0.003,  7.24,   8.11,   9.87,  // Top row (random near 0 and top right)
+            0.004,  0.002,  5.18,   6.92,   4.23,  // Second row
+            10.01,  11.45,  0.23,   13.76,  14.54, // Third row (random middle)
+            20.12,  19.67,  17.35,  1.004,  1.002, // Fourth row (bottom right near 1)
+            21.11,  23.05,  22.98,  1.001,  1.003  // Fifth row
         ]);
         let nmap : DMatrix::<i32>;
         let num_neighborhoods : i32;
 
-        (nmap, num_neighborhoods) = seathru::construct_neighborhood_map(&depth_matrix, 0.03);
+        (nmap, num_neighborhoods) = seathru::construct_neighborhood_map(&depth_matrix, 0.3);
 
         println!("Depth Matrix: ");
         for (_, row) in depth_matrix.row_iter().enumerate() {
