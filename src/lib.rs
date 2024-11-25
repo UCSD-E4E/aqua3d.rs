c6809c90b09c454dccc98867c8479b54907e3965
Avik Ghosh <a.ghosh24@outlook.com>
2024-11-24 20:13:34 -0800

    feat: Rewrite using anyhow, ndarray, adding random flag for testing/production code in construct_neighborhood_map

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,19 @@ pub fn add(left: u64, right: u64) -> u64 {
 
 #[cfg(test)]
 mod tests {
-    use nalgebra::DMatrix;
+    use core::num;
+    // Includes
+    #[allow(unused)]
+    use std::collections::VecDeque;
+    use anyhow::{Context, Result};
 
+    use itertools::Dedup;
+    use nalgebra::DimName;
+    #[allow(unused)]
+    use num_traits::ToPrimitive;
+
+    #[allow(unused)]
+    use ndarray::{array, Array1, Array2};
     use super::*;
 
     #[test]
@@ -17,41 +28,21 @@ mod tests {
     }
 
     #[test]
-    fn test_construct_neighborhood_map() {
-        // let depth_matrix = DMatrix::<f32>::from_data(data);
-        let depth_matrix = DMatrix::<f32>::from_row_slice(5, 5, &[
-            0.001,  0.003,  7.24,   8.11,   9.87,  // Top row (random near 0 and top right)
-            0.004,  0.002,  5.18,   6.92,   4.23,  // Second row
-            10.01,  11.45,  0.23,   13.76,  14.54, // Third row (random middle)
-            20.12,  19.67,  17.35,  1.004,  1.002, // Fourth row (bottom right near 1)
-            21.11,  23.05,  22.98,  1.001,  1.003  // Fifth row
-        ]);
-        let nmap : DMatrix::<i32>;
-        let num_neighborhoods : i32;
+    fn construct_neighborhood_map_test() -> Result<()> {
+        let depth_matrix = array![
+            [0.001,  0.003,  7.24,   8.11,   9.87],  // Top row (random near 0 and top right)
+            [0.004,  0.002,  5.18,   6.92,   4.23],  // Second row
+            [10.01,  11.45,  0.23,   13.76,  14.54], // Third row (random middle)
+            [20.12,  19.67,  17.35,  1.004,  1.002], // Fourth row (bottom right near 1)
+            [21.11,  23.05,  22.98,  1.001,  1.003]  // Fifth row
+        ];
 
-        (nmap, num_neighborhoods) = seathru::construct_neighborhood_map(&depth_matrix, 0.3);
+        let (nmap, num_neighborhoods) = seathru::construct_neighborhood_map(&depth_matrix, 0.1, false)
+                                                                                    .context("Failed to set up neighborhood map")?;
 
-        println!("Depth Matrix: ");
-        for (_, row) in depth_matrix.row_iter().enumerate() {
-            print!("[");
-            for (_, column) in row.into_iter().enumerate() {
-                print!("{}\t", column);
-            }
-            println!("]");
-        }
-        println!();
-        println!("Neighborhood Map: ");
+        println!("{:?}", nmap);
 
-        for (_, row) in nmap.row_iter().enumerate() {
-            print!("[");
-            for (_, column) in row.into_iter().enumerate() {
-                print!("{}\t", column);
-            }
-            println!("]");
-        }
-        // dbg!(depth_matrix);
-        // dbg!(nmap);
-        println!("There were {} neighborhoods!", num_neighborhoods)
+        Ok(())
+    }   
 
-    }
 }
--- src/seathru.rs
+++ src/seathru.rs
@@ -6,138 +6,94 @@
 ///           To be used by FishSense as library for data processing.
 
 // Includes
-extern crate nalgebra as na;
-use na::DMatrix;
+#[allow(unused_imports)]
+use rand::Rng;
+use num_traits::ToPrimitive;
+use ndarray_stats::QuantileExt;
+use ndarray::{array, Array, Array2};
 
 use std::collections::VecDeque;
-use num_traits::ToPrimitive;
-use rand::Rng;
+use itertools::Itertools;
 
-// Allow for addition of indices
-macro_rules! add_tuples {
-    ($t1:expr, $t2:expr) => {
-        ($t1.0 + $t2.0, $t1.1 + $t2.1)
-    };
-}
+use anyhow::{Context, Result};
 
-// Extend DMatrix
-trait NumPyMethods {
-    fn np_where(&self, val: i32) -> Vec<(usize, usize)>;
-    fn np_unique_cnts(&self) -> Vec<(i32, usize)>;
-    fn is_within_bounds(&self, index : (i32, i32)) -> bool;
-}
+#[allow(dead_code, unused)]
+pub fn construct_neighborhood_map(depth_map: &Array2<f32>, eps: f32, is_rand : bool) -> Result<(Array2<i32>, i32)> {
+    let scaled_eps = (*depth_map.max()? - *depth_map.min()?) * eps;
+    let mut neighborhood_map : Array2<i32> = Array::zeros(depth_map.raw_dim());         // Convert to u32 soon
+    let mut indices_left = neighborhood_map.indexed_iter();
 
-impl NumPyMethods for DMatrix<i32> 
-// where 
-    // T: RealField + ToPrimitive,
-{
-    fn np_where(&self, val: i32) -> Vec::<(usize, usize)> {
-        let mut inds = Vec::<(usize, usize)>::new();
-        let mut ind = 0;
-
-        let _ = DMatrix::from_fn(self.nrows(), self.ncols(), |i, j| {
-            let mat_val = self[(i, j)];
-            if mat_val.to_i32() == Some(val) { inds.insert(ind, (i, j)); ind += 1; }
-        });
-        
-        inds
-    }
-
-    fn np_unique_cnts(&self) -> Vec<(i32, usize)> {
-        let mut vals = Vec::<i32>::new();
-        let mut counts = Vec::<usize>::new();
-
-        let _ = DMatrix::from_fn(self.nrows(), self.ncols(), |i, j| {
-            let mat_val = self[(i, j)];
-            if vals.contains(&mat_val) { 
-                counts[vals.iter().position(|&val| val == mat_val).unwrap() as usize] += 1; 
-            } else {
-                vals.push(mat_val);
-                counts.push(1);
-            }
-        });
-
-        let unique = vals.iter().zip(counts.iter())
-                .map(|(&x, &y)| (x, y))
-                .collect();
-        unique    
-    } 
-
-    fn is_within_bounds(&self, index : (i32, i32)) -> bool {
-        if index.0 < 0 || index.1 < 0 {
-            false
-        } else if index.0 >= (self.nrows() as i32) || index.1 >= (self.ncols() as i32) {
-            false
-        } else {
-            true
-        }
-    }
-}
-
-// Function takes in depth map (2D array) and groups them into neighborhoods of deviation
-// epsilon. In other words, discretize the depth map with epsilon as an approximate precision
-// Each 'neighborhood' is computed to be in the same epsilon-neighborhood.
-#[allow(dead_code)]
-pub fn construct_neighborhood_map(depth_map: &DMatrix::<f32>, epsilon: f32) -> (DMatrix::<i32>, i32) {
-    // Copying original functionality
-    let scaled_eps = (depth_map.max() - depth_map.min()) * epsilon;
-    let mut neighborhood_map = DMatrix::<i32>::zeros(depth_map.shape().0, depth_map.shape().1);
     let mut num_neighborhoods = 1;
 
-    // Can convert into .min() == 0 for memory efficiency
-    while !neighborhood_map.np_where(0).is_empty() {                         
-        let zero_map = neighborhood_map.np_where(0);
-        let start_index = rand::thread_rng().gen_range(0..zero_map.len());
-        let (start_x, start_y) = zero_map[start_index];
+    // Discretization loop
+    // Condition is simple way to make sure the boolean map of zero values isn't empty
+    while *neighborhood_map.min()? == 0 {           
+        let zero_map : Vec<(usize, usize)> = neighborhood_map.indexed_iter()
+                                                                .filter(|&((_, _), &value)| value == 0)
+                                                                .map(|(index, _)| index)
+                                                                .collect();
+
+        let (start_x, start_y) = match is_rand {
+            true => zero_map[rand::thread_rng().gen_range(0..zero_map.len())],
+            false => zero_map[0 as usize]
+        };
 
         let mut q = VecDeque::<(usize, usize)>::new();
         q.push_back((start_x, start_y));
 
-        let mut x_ind : i32;
-        let mut y_ind : i32;
-        while q.len() != 0 {
-            let first_val = q.pop_front().unwrap();
-            (x_ind, y_ind) = (first_val.0 as i32, first_val.1 as i32);
+        let mut x_ind : usize;
+        let mut y_ind : usize;
 
-            if (depth_map[(x_ind as usize, y_ind as usize)] - depth_map[(start_x, start_y)]).abs() <= scaled_eps {
-                neighborhood_map[(x_ind as usize, y_ind as usize)] = num_neighborhoods;
+        while q.len() != 0 {
+            (x_ind, y_ind) = q.pop_front().context("Empty queue")?;
+
+            if (depth_map[(x_ind, y_ind)] - depth_map[(start_x, start_y)]).abs() <= scaled_eps {
+                neighborhood_map[(x_ind, y_ind)] = num_neighborhoods;
 
                 // Add in neighboring points. x_ind and y_ind are already positive or 0, so no need to check that.
-                // List of Possible Deltas from (x_ind, y_ind) to have the 4 surrounding (non-diagonal) points.
-                let deltas = vec![(-1, 0), (1, 0), (0, -1), (0, 1)];
+                // Array of arrays for all Deltas from (x_ind, y_ind) to have the 4 surrounding (non-diagonal) points.
+                let deltas = array![(-1, 0), (1, 0), (0, -1), (0, 1)]
+                                                                            .map(|x| array![x.0 as i32, x.1 as i32]);
+
                 for delta in deltas {
-                    let (del_x_ind, del_y_ind) = add_tuples!((x_ind, y_ind), delta);
-                    if (neighborhood_map.is_within_bounds((del_x_ind, del_y_ind))) &&       // Needs to short circuit
-                        neighborhood_map[(del_x_ind as usize, del_y_ind as usize)] == 0 {
-                            q.push_back((del_x_ind as usize, del_y_ind as usize));
+                    let surr_inds = (&array![x_ind as i32, y_ind as i32] + &delta);
+
+                    let [del_x_ind, del_y_ind] = *surr_inds.as_slice().expect("Array should be contiguous and of size 2") else {
+                        todo!("Please handle the case where the array is not of size 2!");
+                    };
+
+                    if let (Some(x), Some(y)) = (del_x_ind.to_usize(), del_y_ind.to_usize()) {
+                        if let Some(val) = neighborhood_map.get((x, y)) {
+                            if (*val == 0) { q.push_back((x, y)); }
+                        }
                     }
                 }
             }
         }
-        num_neighborhoods += 1; // Move onto the next neighborhood
+
+        // Increment
+        num_neighborhoods += 1;
     }
 
     // TODO: Why are we doing equality on floating point?? is depth integer then? Currently using 
-    let practically_zero = 0.5;
-    let zero_depth = depth_map.zip_map(&depth_map, |a, _b| a.abs() < practically_zero);
-    let nmap_on_zero_depth = neighborhood_map.zip_map
-    (
-        &zero_depth, 
-        |nmap_value, is_zero| {
-            if is_zero { nmap_value } else { -1 }
-        }
-    );
+    let practically_zero = *depth_map.min()? + 1e5;
 
-    // let mut vals_w_counts = nmap_on_zero_depth.np_unique_cnts();
-    let mut vals_w_counts = nmap_on_zero_depth.np_unique_cnts();
-    vals_w_counts.retain(|&(nmap_val, _)| nmap_val != -1);
-    vals_w_counts.sort_by_key(|tup| tup.1);                      // Sort in ascending order on count
-    vals_w_counts.reverse();                                                    // Reverse so max count at beginning
+    // Boolean map for depth indices that are 'practically zero', get vector of indices whose depths are 
+    // 'practically zero', then return iterator of the neighborhood_map values at those indices, collect() on iterator.
+    let zero_depth_nmap : Vec<i32> = depth_map.map(|x| *x < practically_zero)
+                                                                    .indexed_iter()
+                                                                    .filter(|&((x, y), &is_zero)| is_zero)
+                                                                    .map(|((x, y), _)| neighborhood_map[(x, y)])
+                                                                    .collect();
 
-    if vals_w_counts.len() > 0 { 
-        // Reset largest nmap count to be 0
-        neighborhood_map = neighborhood_map.zip_map(&neighborhood_map, |nmap_val, _b| if nmap_val == vals_w_counts[0].0 { 0 } else { nmap_val } );
-    }
-    
-    (neighborhood_map, num_neighborhoods - 1)
+
+    // Most common nmap value for 0 depth can be found in counts
+    let mut counts = zero_depth_nmap.iter().counts();
+    let (most_frequent, _) = dbg!(counts.into_iter()
+                                    .max_by_key(|&(_, count)| count)
+                                    .expect("Empty depth matrix"));
+
+    // TODO: Figure out why we want a gap in the neighborhood mapping.
+    Ok((neighborhood_map.map(|&nmap_val| if (nmap_val == *most_frequent) { 0 } else { nmap_val }), 
+        num_neighborhoods - 1))
 }
